dd7084cf7d5b76ef015af418d80a55a7d54f04a4
jagarian1 <jagarian@naver.com>
2022-12-26 23:22:41 +0900

    33333333

--- src/R006_LifecycleEx.js
+++ src/R006_LifecycleEx.js
@@ -1,22 +1,22 @@
 import React, { Component } from 'react';
 
 class R006_LifecycleEx extends Component {
-  static getDerivedStateFromProps(props,state) {
-    console.log('2. getDerivedStateFromProps Call :'+props.prop_value);
-    return {};
-  }
-  constructor(props) {
-    super(props);
-    this.state = {};
-    console.log('1. constructor Call::::', props.prop_value);
-  }
-
-  render() {
-    console.log('3. render Call');
-    return (
-      <h2>[ THIS IS CONSTRUCTOR FUCNTION ] </h2>
-    )
-  }
+    static getDerivedStateFromProps(props,state) {
+        console.log('2. getDerivedStateFromProps Call :'+props.prop_value);
+        return {};
+    }
+    constructor(props) {
+        super(props);
+        this.state = {};
+        console.log('1. constructor Call::::', props.prop_value);
+    }
+     
+    render() {
+        console.log('3. render Call');
+        return (
+        <h2>[ THIS IS CONSTRUCTOR FUCNTION ] </h2>
+        )
+    }
 }
 
 export default R006_LifecycleEx;
