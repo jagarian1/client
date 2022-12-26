4b95f573c32ef08b97667e631044d10c7c002145
jagarian@naver.com <jagarian@naver.com>
2022-12-27 00:37:31 +0900

    '12-27:1237'

--- src/App.js
+++ src/App.js
@@ -4,10 +4,8 @@
 import React from 'react';
 import './App.css';
 //import ImportComponent from './R003_ImportComponent';
-//import LifecycleEx from './R004_LifecycleEx';
-//import LifecycleEx from  './R005_LifecycleEx';
-//import LifecycleEx from  './R006_LifecycleEx';
-import LifecycleEx from './R007_LifecycleEx';
+// import LifecycleEx from './R007_LifecycleEx';
+import LifecycleEx from './R008_LifecycleEx';
 
 function App() {
   return (
@@ -23,13 +21,27 @@ function App() {
     </div>
   );
 }
-/*
-C:\react200\client>git add .
-warning: LF will be replaced by CRLF in src/App.js.
-The file will have its original line endings in your working directory  
- * 경고 : bora.txt에서 LF는CRLF로 대체됩니다.
-파일은 작업 디렉토리에 원래 줄 끝이 있습니다.
-해결 : git config --global core.autocrlf true
- * 
- */
+
 export default App;
+/*
+  * C:\react200\client>git add .
+  * warning: LF will be replaced by CRLF in src/App.js.
+  * The file will have its original line endings in your working directory  
+  * 경고 : bora.txt에서 LF는CRLF로 대체됩니다.
+  * 파일은 작업 디렉토리에 원래 줄 끝이 있습니다.
+  * 
+  * 해결 : git config --global core.autocrlf true
+  * 
+  * git config --global core.autocrlf true //input 이건 리눅스에서 한방향으로 적용 할떄 옵션을 줍니다.
+  * 
+  * git 각종 명령어
+  * git init 
+  * git branch localID 
+  * git switch origin localID
+  * git add .
+  * git commit -m 'UpdatContents' //띠어쓰기하면 오류닙니다.
+  * git status --상태보기
+  * git log --all --oneline //한줄로 로그 보기
+  * git pull origin localId OR git pull origin master
+  * git push origin master  //해당 브렌치로 작동
+  */
