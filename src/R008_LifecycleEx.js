0e23b0912cbd60f3246231e45f044423d4bc619e
JeromeJR <jagarian@naver.com>
2022-12-29 12:22:24 +0900

    1229React200_0010

--- src/R008_LifecycleEx.js
+++ src/R008_LifecycleEx.js
@@ -2,19 +2,19 @@ import React, { Component } from 'react';
 
 class R008_LifecycleEx extends Component {
   static getDerivedStateFromProps(props, state) {
-    console.log('2. getDerivedStateFromProps Call :'+props.prop_value);
+    console.log('2. getDerivedStateFromProps Call :'+props.prop_value);  //#2
     return {tmp_state:props.prop_value};
   }
 
   constructor(props) {
     super(props);
     this.state = {};
-    console.log('1. constructor Call');
+    console.log('1. constructor Call'); //#1
   }
 
   componentDidMount() {
-    console.log('4. componentDidMount Call');
-    console.log('5. tmp_state : '+this.state.tmp_state);
+    console.log('4. componentDidMount Call');  //#4
+    console.log('5. tmp_state : '+this.state.tmp_state);  //#5
     this.setState({tmp_state2 : true});
   }
 
@@ -24,7 +24,7 @@ class R008_LifecycleEx extends Component {
   }
 
   render() {
-    console.log('3. render Call');
+    console.log('3. render Call');  //#3
     return (
       <h2>[ THIS IS shouldComponentUpdate FUCNTION ]</h2>  //변경 부분 shouldComponentUpdate
     )
---
+++ src/R010_Variable.js
@@ -0,0 +1,35 @@
+import React, { Component} from 'react';
+
+class R010_Varliable extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    componentDisMount() {
+        var varName = 'react';
+        console.log('varNmae1='+varName);
+        //var varName2 = '200' //'varname' is already define no-redeclare 
+        console.log('varNmae2='+varName);
+
+        let letName = 'react';
+        console.log('letName1='+letName);
+        
+        // let letName = '200' // Parsing error: Identifier 'letName' has already been declared
+        letName = 'react200';
+        
+        const constName = 'react';
+        console.log('constName='+constName);
+        //const constName = '200'; //error: uncaught TypeError :
+
+    }
+
+    render() {
+        return (
+            <h2>[THis is Valiable ]</h2>
+        )
+    }
+}
+
+export default R010_Varliable;
